44341e167c91f37d54f89859fa1a4bf810830048
RedeemedSpoon <142355786+RedeemedSpoon@users.noreply.github.com>
2023-11-16 22:02:46 +0000

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,4 +2,4 @@ browser = "Firefox"  # Choose only between 'Firefox', 'Chrome', 'Edge', 'Safari'
 get_sugar_lump_early = False # Will the sugar lump 3 hour earlier but has 50% to harvest it
 get_golden_warth_cookie = False # This setting should be off if in grandmapocalypse
 end_grandmapocalypse = False # Personal Preference
-wizzard_bonus = "Conjure Baked Goods"  # Can only be set to 'Force the Hand of Fate', 'Conjure Baked Goods' or 'Spondaneous Edifice'
+wizzard_bonus = "Conjure Baked Goods"  # Can only be set to 'Force the Hand of Fate', 'Conjure Baked Goods' or 'Spontaneous Edifice'
